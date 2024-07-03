e663405e6b1a9fdb4e172b2f446139d69c486321
Steve Chesley <steve.chesley@jpl.nasa.gov>
2024-07-02 18:12:27 -0700

    Debugging, detesting and delinting...xs

--- lcanalyzer/models.py
+++ lcanalyzer/models.py
@@ -17,6 +17,40 @@ import pandas as pd
 #import numpy as np
 #from astropy.timeseries import LombScargle
 
+def normalize_lc(df,mag_col):
+    """LC normalization.
+    
+    :param df: the DataFrame in question
+    :param mag_col: the name of the DataFrame column in question
+    :returns: array with normalized light curve .
+    """
+    # Normalize a single light curve
+    if any(df[mag_col].abs() > 90):
+        raise ValueError(mag_col+' contains values with abs() larger than 90!')
+    brightest = min_mag(df,mag_col)
+    faintest = max_mag((df-brightest),mag_col)
+    lc = (df[mag_col]-brightest)/faintest
+    lc = lc.fillna(0)
+    return lc
+
+def calc_stats(lc, bands, mag_col):
+    """Calculate basic statistics for a DataFrame.
+    
+    :param lc: the DataFrame in question
+    :param bands: list of bands to query
+    :param mag_col: the name of the DataFrame column in question
+    :returns: DataFrame with statistics: min, mean, max per band.
+    """
+    # Calculate max, mean and min values for all bands of a light curve
+    stats = {}
+    for b in bands:
+        stat = {}
+        stat["max"] = max_mag(lc[b], mag_col)
+        stat["mean"] = mean_mag(lc[b], mag_col)
+        stat["min"] = min_mag(lc[b], mag_col)
+        stats[b] = stat
+    return pd.DataFrame.from_records(stats)
+
 def load_dataset(filename):
     """Load a table from CSV file.
     
